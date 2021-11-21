88bbaf05af62c3ed8dd0ee5fa5ac83e2b7d9becd
OlesiaNazarenko <eightpa8@gmail.com>
2021-11-21 12:44:06 +0200

    style components added

--- src/components/_pages/MainInfo.jsx
+++ src/components/_pages/MainInfo.jsx
@@ -1,44 +1,41 @@
 import PropTypes from 'prop-types';
 
-const MainInfo = ({ title,options, openActivePage, activePage}) => {
-   
-    return (
-        <>
-            <section>
-            <h2>{title}</h2>
-            <p>UAH</p>
-                <button onClick={(e) => { openActivePage(activePage) }} type="button">Add</button>
-                <ul>
-                    {
-                        options.map((el) => {
-                            return (
-                                <li key={el.name}>
-                                    <span>{ el.period}</span>
-                                    <span>{el.sum}</span>
-                                </li>
-                            )
-                        })
-                    }
-                       
-                </ul>
-            </section>
-            </>
-    )
-}
+const MainInfo = ({ title, options, openActivePage, activePage }) => {
+  const handlerClick = e => openActivePage(activePage);
+
+  return (
+    <>
+      <section>
+        <h2>{title}</h2>
+        <p>UAH</p>
+        <button onClick={handlerClick} type="button">
+          Add
+        </button>
+        <ul>
+          {options.map(el => {
+            return (
+              <li key={el.name}>
+                <span>{el.period}</span>
+                <span>{el.sum}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
+};
 
 MainInfo.propTypes = {
-    title:PropTypes.string.isRequired ,
-    options: PropTypes.arrayOf(
-        PropTypes.shape({
-        period: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        sum: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-        ]).isRequired,
-    })
-    ).isRequired,
-    openActivePage: PropTypes.func.isRequired,
-    activePage:PropTypes.string.isRequired ,
-}
+  title: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      period: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      sum: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    }),
+  ).isRequired,
+  openActivePage: PropTypes.func.isRequired,
+  activePage: PropTypes.string.isRequired,
+};
 export default MainInfo;
